cb37d87529e4ccca2c549c7f4eae4a5205a7a96a
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-08-22 01:01:31 +0300

    Create app structure & setup app layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,31 @@
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { Home, CreatePost } from './pages';
+import { logo } from './assets';
+
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <BrowserRouter>
+      <header className="w-full flex justify-between bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4] text-3xl">
+        <Link to="/">
+          <img src={logo} className="w-20 object-contain" />
+        </Link>
+
+        <Link
+          to="/create-post"
+          className="font-inter font-medium bg-[#6469ff] hover:bg-[#595de7] transition-all duration-300 text-white text-sm px-4 py-2 rounded-md"
+        >
+          Create
+        </Link>
+      </header>
+
+      <main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create-post" element={<CreatePost />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
+  );
 };
 
 export default App;
