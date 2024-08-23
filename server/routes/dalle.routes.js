2c01980eda118f1c5a5c25090c55a24d3b35a806
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-08-23 20:19:31 +0300

    Add controller functions to dalle route

--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -1,5 +1,13 @@
-import e from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
 
-const router = e.Router();
+import express from 'express';
+
+import { getHelloMessage, generateImage } from '../controller/dalle.controller.js';
+
+const router = express.Router();
+
+router.route('/').get(getHelloMessage);
+router.route('/').post(generateImage);
 
 export default router;
